3ac7a706697c89e11a0cffe46e9f65e09fe0d45e
Joanna <joanna.czarnecka@hotmail.com>
2020-05-31 15:34:20 +0200

    Stage 5 changes

--- src/encryptdecrypt/Main.java
+++ src/encryptdecrypt/Main.java
@@ -1,5 +1,9 @@
 package encryptdecrypt;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class Main {
@@ -29,7 +33,7 @@ public class Main {
     public static void main(String[] args) {
         String method = "";
         int key = 0;
-        String data = "";
+        String data = "", inPath = "", outPath = "", result = "";
 
         for (int i = 0; i < args.length; i += 2) {
             switch (args[i]) {
@@ -42,22 +46,51 @@ public class Main {
                 case "-data":
                     data = args[i + 1];
                     break;
+                case "-in":
+                    inPath = args[i + 1];
+                    break;
+                case "-out":
+                    outPath = args[i + 1];
+                    break;
                 default:
                     throw new IllegalStateException("Unexpected value: " + args[i]);
             }
         }
 
+        if (data == "") {
+            File file = new File(inPath);
+            try (Scanner scanner = new Scanner(file)) {
+                data = scanner.nextLine();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+
+        }
+
         switch (method) {
             case "":
             case "enc":
-                System.out.println(encrypt(data, key));
+                result = encrypt(data, key);
                 break;
             case "dec":
-                System.out.println(decrypt(data, key));
+                result = decrypt(data, key);
                 break;
             default:
                 System.out.println("method name invalid");
                 break;
         }
+
+        if (outPath != "") {
+            File file = new File(outPath);
+            try (PrintWriter printWriter = new PrintWriter(file)) {
+                printWriter.print(result);
+            } catch (IOException e) {
+                System.out.printf("An exception occurs %s", e.getMessage());
+            }
+
+        } else {
+            System.out.println(result);
+        }
+
     }
 }
